93133dc40e8c0949a4bec725950756e8aaa330cf
Sumit Kumar <iam.sumitkumar64@gmail.com>
2026-05-12 21:55:54 +0530

    follow unfollow event fired and email sent

--- mail-server/src/infrastructure/rabbit-mq/consumers/user/follow-created/follow-created.consumer.ts
+++ mail-server/src/infrastructure/rabbit-mq/consumers/user/follow-created/follow-created.consumer.ts
@@ -1,35 +1,33 @@
 import { Injectable, Logger, OnModuleInit } from '@nestjs/common';
-import { EmailService } from 'src/infrastructure/email/mail.service';
 import { RabbitMQService } from 'src/infrastructure/rabbit-mq/rabbit-mq.service';
 import { ExchangeNameEnum, ExchangeTypeEnum, QueueEnum, RoutingKeyEnum } from 'src/infrastructure/rabbit-mq/type-enum/rabbit-mq.enum';
 import { FollowRepository } from 'src/infrastructure/repository/follow.repo';
 
 @Injectable()
-export class CreatorPostCreatedConsumer implements OnModuleInit {
-    private readonly logger = new Logger(CreatorPostCreatedConsumer.name);
+export class FollowCreatedConsumer implements OnModuleInit {
+    private readonly logger = new Logger(FollowCreatedConsumer.name);
 
     constructor(
         private readonly rabbitMQService: RabbitMQService,
-        private readonly emailService: EmailService,
         private readonly followRepo: FollowRepository,
     ) { }
 
     async onModuleInit() {
         await this.rabbitMQService.setupExchangeQueueAndBind(
-            QueueEnum.MAIL_POST_CREATED_QUEUE,
+            QueueEnum.MAIL_FOLLOW_CREATED_QUEUE,
             ExchangeNameEnum.CREATOR_EXCHANGE,
-            RoutingKeyEnum.CREATOR_POST_CREATED,
+            RoutingKeyEnum.FOLLOW_CREATED,
             ExchangeTypeEnum.TOPIC,
         );
 
         await this.rabbitMQService.consumeMessages(
-            QueueEnum.MAIL_POST_CREATED_QUEUE,
+            QueueEnum.MAIL_FOLLOW_CREATED_QUEUE,
             async (data) => {
-                this.logger.log(`Processing creator post creation: ${data.uuid}`);
+                this.logger.log(`Processing follow created: ${data.uuid}`);
 
-                const followers = await this.followRepo.findByFollowingUuid(data.user_uuid);
-                for (const follower of followers) {
-                    await this.emailService.sendCreatorPostNotification(follower.follower, data);
+                const follow = await this.followRepo.findByUuid(data.uuid);
+                if (!follow) {
+                    await this.followRepo.createFollowerBond(data);
                 }
             },
         );
--- mail-server/src/infrastructure/rabbit-mq/consumers/user/follow-deleted/follow-deleted.consumer.ts
+++ mail-server/src/infrastructure/rabbit-mq/consumers/user/follow-deleted/follow-deleted.consumer.ts
@@ -1,36 +1,31 @@
 import { Injectable, Logger, OnModuleInit } from '@nestjs/common';
-import { EmailService } from 'src/infrastructure/email/mail.service';
 import { RabbitMQService } from 'src/infrastructure/rabbit-mq/rabbit-mq.service';
 import { ExchangeNameEnum, ExchangeTypeEnum, QueueEnum, RoutingKeyEnum } from 'src/infrastructure/rabbit-mq/type-enum/rabbit-mq.enum';
 import { FollowRepository } from 'src/infrastructure/repository/follow.repo';
 
 @Injectable()
-export class CreatorPostCreatedConsumer implements OnModuleInit {
-    private readonly logger = new Logger(CreatorPostCreatedConsumer.name);
+export class FollowDeletedConsumer implements OnModuleInit {
+    private readonly logger = new Logger(FollowDeletedConsumer.name);
 
     constructor(
         private readonly rabbitMQService: RabbitMQService,
-        private readonly emailService: EmailService,
         private readonly followRepo: FollowRepository,
     ) { }
 
     async onModuleInit() {
         await this.rabbitMQService.setupExchangeQueueAndBind(
-            QueueEnum.MAIL_POST_CREATED_QUEUE,
+            QueueEnum.MAIL_FOLLOW_DELETED_QUEUE,
             ExchangeNameEnum.CREATOR_EXCHANGE,
-            RoutingKeyEnum.CREATOR_POST_CREATED,
+            RoutingKeyEnum.FOLLOW_DELETED,
             ExchangeTypeEnum.TOPIC,
         );
 
         await this.rabbitMQService.consumeMessages(
-            QueueEnum.MAIL_POST_CREATED_QUEUE,
+            QueueEnum.MAIL_FOLLOW_DELETED_QUEUE,
             async (data) => {
-                this.logger.log(`Processing creator post creation: ${data.uuid}`);
+                this.logger.log(`Processing follow deleted: ${data.uuid}`);
 
-                const followers = await this.followRepo.findByFollowingUuid(data.user_uuid);
-                for (const follower of followers) {
-                    await this.emailService.sendCreatorPostNotification(follower.follower, data);
-                }
+                await this.followRepo.deleteFollowBond(data.uuid);
             },
         );
     }
